08fe40c40c9fd400a6065a12b4407bb13690833e
Mkmihir16 <132197042+Mkmihir16@users.noreply.github.com>
2023-10-24 01:18:17 +0530

    changes

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 
 var tl=gsap.timeline()
+function time()
+{
+    var a=0;
+      setInterval(function(){
+       a+=Math.floor(Math.random()*20);
+       if(a<100)
+       {
+       document.querySelector(".loader h1").innerHTML=a+"%";
+       }
+       else{
+        a=100;
+        document.querySelector(".loader h1").innerHTML=a+"%"
+       }
+    }
+    ,150)
+}
+
+
+tl.to(".loader",{
+    top:"-100vh",
+    duration:2,
+    delay:2
+   
+})
+tl.to(".loader h1",{
+    
+    duration:1,
+    delay:2,
+    onStart:time()
+})
 tl.from(".heading h1",{
     
     opacity:0.2,
